058e18696a3de865d635633d7875cab7db62a56d
BingYinBest <dreamfy66@outlook.com>
2026-09-21 11:59:22 +0800

    Change applicationId from 'com.tencent.qqmusic' to 'com.heytap.music'

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,7 @@ android {
     namespace = "me.bmax.apatch"
 
     defaultConfig {
-        applicationId = "com.tencent.qqmusic"
+        applicationId = "com.heytap.music"
     }
 
     buildTypes {
